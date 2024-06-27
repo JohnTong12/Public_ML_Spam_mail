24af30714170f9955204cb3117ec6a1d1c8e23e0
JohnTong12 <garabinodeng7@gmail.com>
2024-06-27 13:56:07 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,17 +1,3 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Jun 21 16:38:13 2024
-
-@author: JT
-"""
-
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Jun 20 22:30:19 2024
-
-@author: JT
-"""
-
 import numpy as np
 import streamlit as st
 import nltk
@@ -24,37 +10,16 @@ import string
 porter_stem = PorterStemmer()
 
 def stemming(message):
-  """
-  Preprocesses the message text for better spam detection:
-
-  - Removes non-alphanumeric characters
-  - Removes URLs and HTML tags
-  - Removes digits
-  - Removes punctuation
-  - Removes newlines
-  - Removes words containing alphanumeric and digits (e.g., "ph0n3")
-  - Converts to lowercase
-  - Performs stemming using PorterStemmer
-  - Removes stop words
-
-  Args:
-      message (str): The message text to be preprocessed.
-
-  Returns:
-      str: The preprocessed message text.
-  """
-
-  stemmed_content = re.sub(r'[^\w\s]', '', message)  # Remove non-alphanumeric characters
-  stemmed_content = re.sub(r'\[.*?\]|https?://\S+|www\.\S+|<.*?>+', '', stemmed_content)  # Remove URLs, HTML tags, and digits
+  stemmed_content = re.sub(r'[^\w\s]', '', message) 
+  stemmed_content = re.sub(r'\[.*?\]|https?://\S+|www\.\S+|<.*?>+', '', stemmed_content)  
   stemmed_content = re.sub(r'\w*\d\w*', '', stemmed_content)
-  stemmed_content = re.sub(r'[{}]'.format(re.escape(string.punctuation)), '', stemmed_content)  # Remove punctuation
+  stemmed_content = re.sub(r'[{}]'.format(re.escape(string.punctuation)), '', stemmed_content)  
   stemmed_content = stemmed_content.lower().split()
   stemmed_content = [porter_stem.stem(word) for word in stemmed_content if word not in stopwords.words('english')]
-  stemmed_content = " ".join(stemmed_content)  # Join stemmed words
+  stemmed_content = " ".join(stemmed_content)  
 
   return stemmed_content
 
-# Loading the model and TF-IDF vectorizer (assuming they exist in your file structure)
 loaded_model = pickle.load(open('model.pkl', 'rb'))
 tfidf = pickle.load(open('feature_extraction.pkl','rb'))
 
